6e64557fb8c00d716c701ff7481a57dead9a0c79
Emma Wang <259150405+EJW0@users.noreply.github.com>
2026-02-08 20:30:14 -0500

    changed to light theme

--- src/main/java/edu/cwru/passwordmanager/PasswordApplication.java
+++ src/main/java/edu/cwru/passwordmanager/PasswordApplication.java
@@ -14,9 +14,8 @@ public class PasswordApplication extends Application {
     static Stage primaryStage = null;
     @Override
     public void start(Stage stage) throws IOException {
-        // TODO: Select Preferred
-        //  Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+        //Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
 
         FXMLLoader fxmlLoader = new FXMLLoader(PasswordApplication.class.getResource("initial-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
